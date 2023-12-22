7d5e7162c0c0b3bf3fe461ded8491b8e32e33ca2
Jus <>
2023-12-22 12:58:01 -0500

    text form result

---
+++ lib/widgets/text_form_result.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class TextFormResult extends StatefulWidget {
+  final String label;
+  final String content;
+  const TextFormResult({
+    super.key,
+    required this.label,
+    required this.content,
+  });
+  _TextFormResult createState() => _TextFormResult();
+}
+
+class _TextFormResult extends State<TextFormResult> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = widget.content;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 150,
+            child: Text(
+              widget.label,
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextFormField(
+              controller: _controller,
+              enabled: false,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[300],
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(color: Colors.white, width: 0),
+                ),
+              ),
+              style: const TextStyle(color: Colors.black, fontSize: 13),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
